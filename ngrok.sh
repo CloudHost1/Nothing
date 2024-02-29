83bf69fa860fc7e90f0c5e104122243082680f24
PufferHosting <113721129+PufferHosting@users.noreply.github.com>
2024-02-29 15:04:31 +0200

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 # FOR x86 servers using linux : 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken cr_2JN0YHB3v94GEMrvuFmlgVKcdto #Put Yours here
+./ngrok config add-authtoken 2d2h0omY7uvK8tifX15uy5BKVB3_3bDF14bAM4a9zcKS33L6b #Put Yours here
 ./ngrok http 8080
